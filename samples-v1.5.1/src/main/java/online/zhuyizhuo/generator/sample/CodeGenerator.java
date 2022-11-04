e9c1fc5caf4b428603e149d1147431092ef76578
zhuyizhuo <zhu.yizhuo@163.com>
2022-11-04 15:42:04 +0800

    生成器使用示例
    生成器使用示例

--- samples-v1.5.1/src/main/java/online/zhuyizhuo/generator/sample/CodeGenerator.java
+++ samples-v1.5.1/src/main/java/online/zhuyizhuo/generator/sample/CodeGenerator.java
@@ -5,12 +5,13 @@ import com.github.zhuyizhuo.generator.enums.ModuleTypeEnums;
 import com.github.zhuyizhuo.generator.mybatis.generator.Generator;
 import com.github.zhuyizhuo.generator.mybatis.generator.GeneratorBuilder;
 import com.github.zhuyizhuo.generator.mybatis.generator.service.template.freemarker.impl.MybatisPlusGenerateImpl;
+import org.apache.ibatis.type.JdbcType;
 
 public class CodeGenerator {
 
     public static void main(String[] args) {
         /** 要生成的表名 多个可用英文逗号隔开; 缺省为当前表空间下所有表; */
-        String tableName = "ORDER_DRIVER";
+        String tableName = "ACT_EVT_LOG";
         //TODO 使用时注意修改作者名称
         String author = "zhuo";
         MybatisPlusGenerateImpl mybatisPlusGenerate = new MybatisPlusGenerateImpl();
@@ -18,9 +19,11 @@ public class CodeGenerator {
                 .addGenerateService(mybatisPlusGenerate)
                 .properties("generate.table-names=" + tableName)
                 .properties("generate.java.comment.author=" + author)
+                .fieldType2JdbcType("longblob", JdbcType.VARCHAR)
                 .replaceDefaultTemplate(ModuleTypeEnums.MODEL, TemplateConstants.MODEL_MYBATIS_PLUS_LOMBOK)
                 .replaceDefaultTemplate(ModuleTypeEnums.VO, TemplateConstants.VO_SWAGGER_LOMBOK)
-                .build("simple_config_oracle.properties");
+//                .build("simple_config_oracle.properties");
+                .build("simple_config_mysql.properties");
         generator.generate();
     }
 }
